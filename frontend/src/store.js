aa87bda12dd54307cef9611f5cbf33c2d943b78e
Fahad Altuhafi <faltuhafi@outlook.com>
2021-07-01 17:38:57 -0700

    Update User Profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -10,6 +10,7 @@ import {
   userDetailsReducer,
   userLoginReducer,
   userRegisterReducer,
+  userUpdateProfileReducer,
 } from './reducers/userReducers'
 
 const reducers = combineReducers({
@@ -19,6 +20,7 @@ const reducers = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
