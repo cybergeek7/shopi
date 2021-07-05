549a97780a364f78590abea9faa8627fd9f083c9
Fahad Altuhafi <faltuhafi@outlook.com>
2021-07-05 13:12:12 -0700

    Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 import colors from 'colors'
 
@@ -24,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 
