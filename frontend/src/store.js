8221429c91b62fe1c3b96fac59f1bc8281f590ec
Fahad Altuhafi <faltuhafi@outlook.com>
2021-07-07 22:06:18 -0700

    Order Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,10 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from './reducers/orderReducers'
 
 const reducers = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducers = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 })
 
 const userInfoFromStorage = localStorage.getItem('userInfo')
