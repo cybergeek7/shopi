14c4e65e2d720520b24854e455c2493e64862dbe
Fahad Altuhafi <faltuhafi@outlook.com>
2021-07-13 17:12:03 -0700

    Admin Create Product

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,6 +7,9 @@ import {
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
   PRODUCT_DELETE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
 } from '../constatnts/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -49,9 +52,7 @@ export const listProductDetails = (productId) => async (dispatch) => {
 
 export const deleteProduct = (productId) => async (dispatch, getState) => {
   try {
-    dispatch({
-      type: PRODUCT_DELETE_REQUEST,
-    })
+    dispatch({ type: PRODUCT_DELETE_REQUEST })
 
     const {
       userLogin: { userInfo },
@@ -69,6 +70,34 @@ export const deleteProduct = (productId) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: PRODUCT_CREATE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.post('/api/products', {}, config)
+
+    dispatch({ type: PRODUCT_CREATE_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
