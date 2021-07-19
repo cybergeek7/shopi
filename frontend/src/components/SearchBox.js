2a2ac835d19d2f443f1197622aeedd7dcd98bf9b
Fahad Altuhafi <faltuhafi@outlook.com>
2021-07-18 22:20:54 -0700

    Prepare for deployment

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -16,13 +16,12 @@ const SearchBox = () => {
     }
   }
   return (
-    <Form onSubmit={submitHandler} inline>
+    <Form onSubmit={submitHandler} className='d-flex'>
       <Form.Control
         type='text'
         name='q'
         onChange={(e) => setKeyword(e.target.value)}
         placeholder='Search Products...'
-        className='mr-sm-2 ml-sm-5'
       ></Form.Control>
       <Button type='submit' variant='outline-success' className='p-2'>
         Search
